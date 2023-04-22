93d3d4ac386bd48b14202eb3cc6034d523ccf8b1
Neillcllghn <neillcllghn@outlook.com>
2023-04-22 14:21:06 +0000

    Updating the update and delete functions in views.py

--- booking/views.py
+++ booking/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import generic, View
 from datetime import datetime, timedelta
+from .models import Booking
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.views.generic.edit import FormView, CreateView
 from .forms import BookingForm
 
 # this is to create an index/home page view
@@ -41,22 +45,24 @@ class BookingCreate(CreateView):
         form.add_error(None, 'Ups.....Something went wrong')
         return super().form_invalid(form)
 
-    def update_booking(request, booking_id):
-        booking = get_object_or_404(Booking, id=booking_id)
-        if request.method == 'POST':
-            form = BookingForm(request.POST, instance=booking)
-            if form.is_valid():
-                messages.add_message(request, messages.INFO,
-                                     'Booking was updated successfully')
-                form.save()
-                return redirect('bookings')
-        form = BookingForm(instance=booking)
-        context = {
-            'form': form
-        }
-        return render(request, 'update_bookings.html', context)
 
-    def delete_booking(request, booking_id):
-        booking = get_object_or_404(Booking, id=booking_id)
-        booking.delete()
-        return redirect('bookings')
+def update_booking(request, booking_id):
+    booking = get_object_or_404(Booking, id=booking_id)
+    if request.method == 'POST':
+        form = BookingForm(request.POST, instance=booking)
+        if form.is_valid():
+            messages.add_message(request, messages.INFO,
+                                 'Booking was updated successfully')
+            form.save()
+            return redirect('bookings')
+    form = BookingForm(instance=booking)
+    context = {
+        'form': form
+    }
+    return render(request, 'update_bookings.html', context)
+
+
+def delete_booking(request, booking_id):
+    booking = get_object_or_404(Booking, id=booking_id)
+    booking.delete()
+    return redirect('bookings')
