c1483bb9a493e434727dd943f6e34130b19c85f0
Neillcllghn <neillcllghn@outlook.com>
2023-04-22 14:08:17 +0000

    adding a create booking view and template

--- booking/views.py
+++ booking/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import generic, View
 from datetime import datetime, timedelta
+from .forms import BookingForm
 
 # this is to create an index/home page view
 
@@ -9,7 +10,7 @@ def index(request):
     return render(request, "index.html")
 
 
-# This class view allows user to view their bookings in list format
+# This class view allows user to view their bookings in list format.
 
 
 class BookingList(generic.ListView):
@@ -18,3 +19,24 @@ class BookingList(generic.ListView):
 
     def get_queryset(self):
         return Booking.objects.filter(user=self.request.user)
+
+# View to create a booking.
+
+
+class BookingCreate(CreateView):
+    model = Booking
+    template_name = 'create_bookings.html'
+    form_class = BookingForm
+
+    def get_success_url(self):
+        return self.request.path
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        messages.add_message(self.request, messages.INFO,
+                             'Booking was made successfully')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        form.add_error(None, 'Ups.....Something went wrong')
+        return super().form_invalid(form)
