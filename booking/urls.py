0ed8c846d7e2eebd351a7e723494442e8ab49eb4
Neillcllghn <neillcllghn@outlook.com>
2023-04-22 14:05:14 +0000

    Adding Booking list view and template

--- booking/urls.py
+++ booking/urls.py
@@ -4,7 +4,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.index, name='home'),
-    # path('bookings/', views.BookingList.as_view(), name='bookings'),
+    path('bookings/', views.BookingList.as_view(), name='bookings'),
     # path('create/', views.BookingCreate.as_view(), name='create_bookings'),
     # path('update/<booking_id>', views.update_booking, name='update_bookings'),
     # path('delete/<booking_id>', views.delete_booking, name='delete'),
